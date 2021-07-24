a6e094a8113bb8aaa42022b098508aed1d92e12f
NikolayPolegaev <N5445@yandex.ru>
2021-07-24 13:17:20 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,21 +5,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
+        pageParser(5);
+    }
 
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-
-        Elements table = doc.select(".forumTable").select("tbody").select("tr");
-        //System.out.println(table);
-        for (Element tr : table) {
-            Elements row = tr.select(".postslisttopic");
-            Element thDate = tr.child(5);
-            for (Element td : row) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(thDate.text());
+    private static void pageParser(int amount) throws IOException {
+        for (int i = 0; i < amount; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + amount).get();
+            Elements table = doc.select(".forumTable").select("tbody").select("tr");
+            //System.out.println(table);
+            for (Element tr : table) {
+                Elements row = tr.select(".postslisttopic");
+                Element thDate = tr.child(5);
+                for (Element td : row) {
+                    Element href = td.child(0);
+                    System.out.println(href.attr("href"));
+                    System.out.println(href.text());
+                    System.out.println(thDate.text());
+                }
             }
         }
     }
