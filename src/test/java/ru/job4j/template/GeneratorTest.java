90e9477702dac365e5f1c2c211570b93054ebfad
NikolayPolegaev <N5445@yandex.ru>
2021-08-25 05:09:15 +0300

    3. Шаблонизатор

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -20,4 +20,37 @@ public class GeneratorTest {
         String expected = generator.produce(template, args);
         assertThat(expected, is(actual));
     }
+
+    @Test(expected = Exception.class)
+    public void IncorrectArgumentsLess() {
+        Generator generator = new GeneratorTemplate();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        String actual = "I am a Petr Arsentev, Who are you? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        String expected = generator.produce(template, args);
+    }
+
+    @Test(expected = Exception.class)
+    public void IncorrectArgumentsMore() {
+        Generator generator = new GeneratorTemplate();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        String actual = "I am a Petr Arsentev, Who are you? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        args.put("address", "address");
+        String expected = generator.produce(template, args);
+    }
+
+    @Test(expected = Exception.class)
+    public void IncorrectArgumentsNoSuchInTemplate() {
+        Generator generator = new GeneratorTemplate();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        String actual = "I am a Petr Arsentev, Who are you? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("number", "Petr Arsentev");
+        args.put("123", "you");
+        String expected = generator.produce(template, args);
+    }
 }
