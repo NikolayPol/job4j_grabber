de64e3c56527946a7f4cae283b3f2aac009ae67e
NikolayPolegaev <N5445@yandex.ru>
2021-09-13 01:01:52 +0300

    2. Парковка машин [#853 #186602]

--- src/test/java/ru/job4j/design/lsp/cars/ParkingTest.java
+++ src/test/java/ru/job4j/design/lsp/cars/ParkingTest.java
@@ -45,13 +45,24 @@
 //    }
 //
 //    @Test
-//    public void when2TruckPlaced() {
-//        Parking parking = new Parking(2, 1);
-//        PassengerCar passengerCar = new PassengerCar();
-//        Truck truck = new Truck();
-//        parking.addTrucks(truck);
+//    public void when1TruckPlaced() {
+//        //парковка: 2 места для легковых и 0 для грузовых
+//        Parking parking = new Parking(2, 0);
+//        //конструктор класса Truck принимает число занимаемых мест для легковой машины
+//        Truck truck = new Truck(2);
 //        parking.addTrucks(truck);
 //        //1 грузовая заняла 2 места для легковой
-//        assertEquals(parking.getCars, 2);
+//        assertEquals(parking.getCars, 1);
+//    }
+//
+//    @Test
+//    public void when1TruckNotPlaced() {
+//        //парковка: 1 место для легковых и 0 для грузовых
+//        Parking parking = new Parking(1, 0);
+//        //конструктор класса Truck принимает число занимаемых мест для легковой машины
+//        Truck truck = new Truck(2);
+//        parking.addTrucks(truck);
+//        //1 грузовая занимает 2 места для легковой. Грузовик не паркуется.
+//        assertEquals(parking.getCars, 0);
 //    }
 //}
